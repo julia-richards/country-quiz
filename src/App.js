94d061860210f8ac9f5954270123bb3225496bcb
julia-richards <julia.m.richards@gmail.com>
2020-04-10 12:12:15 -0600

    Implement v1 country select
    This uses a react useState hook to select and active country
    
    For now just the country's geojson is printed to the DOM

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Select from "react-select";
+import countries from "./lib/countries.json";
+
+import countryShapes from "./lib/country_shapes";
+// object of all countries with key of 3 letter code
+// { "SWE": swedenGeoJson }
+
+import "./App.css";
+
+const sortLabels = (a, b) => {
+  if (a.label > b.label) return 1;
+  if (b.label > a.label) return -1;
+
+  return 0;
+};
+
+const countryOptions = countries
+  .map((json) => ({
+    label: json.name.common,
+    value: json.cca3,
+  }))
+  .sort(sortLabels);
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Select
+        value={selectedOption}
+        onChange={(option) => setSelectedOption(option)}
+        options={countryOptions}
+      />
+      <pre>selected option: {JSON.stringify(selectedOption, null, 2)}</pre>
+      {!!selectedOption ? (
+        <pre>
+          active geojson:
+          {JSON.stringify(countryShapes[selectedOption.value], null, 2)}
+        </pre>
+      ) : (
+        <p>Please select country</p>
+      )}
     </div>
   );
 }
